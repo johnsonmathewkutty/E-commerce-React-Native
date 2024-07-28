62c9a6d18a0edf7b616d9edaf0380909d0f63404
Johnson Mathewkutty <johnsonmathew1604@gmail.com>
2024-07-28 19:01:14 +0530

    login page updated

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -1,58 +1,182 @@
-import { StyleSheet, Text, View,TextInput,TouchableOpacity, Alert} from 'react-native'
+import { StyleSheet, Text, View,TextInput,TouchableOpacity,Keyboard,ActivityIndicator} from 'react-native'
 import React, { useState } from 'react'
 
 import auth from '@react-native-firebase/auth'
-import { useDispatch,useSelector } from 'react-redux'
+import firestore from '@react-native-firebase/firestore'
+import { useDispatch } from 'react-redux'
+import Toast from 'react-native-toast-message';
+
 import { firestoreuserid } from '../Redux/Cartreducer'
 
-const Login = ({navigation}) => {
+
+
+const Login = ({navigation,route}) => {
     const [email,setemail]=useState('')
     const [password,setpassword]=useState('')
+    const [loading,setloading]=useState(true)
     const dispatch=useDispatch()
-    const userid=(userId)=>{
-      dispatch(firestoreuserid(userId))
-    }
-    const login=()=>{
+    const login=async()=>{
+      setloading(false)
         if(email !='' && password !=''){
-   auth().signInWithEmailAndPassword(email,password).then((response)=>{
-    Alert.alert('Login sucessfull')
-    userid(response.user.uid)
+            try{
+ const response= await auth().signInWithEmailAndPassword(email,password)
+ Toast.show({
+  type:'success',
+  text1:'Login sucessfull',
+  visibilityTime:1000
+})
+setloading(true)
+       const userId=response.user.uid
+       await datafirestore(userId)
+       await dispatch(firestoreuserid(userId))
     navigation.navigate('Bottomtabs', {
-        screen: 'Home',
-        params: { email: email,
-                 password:password
-        },
+        screen: 'Home'
       })
-   })
-   .catch((Error)=>{
-    Alert.alert(Error.message)
+   }
+   catch(Error){
+    let errormessage='An unknown error occurred'
+    switch(Error.code){
+      case 'auth/invalid-email':
+        errormessage = 'Invalid email address';
+        break;
+        case 'auth/user-disabled':
+          errormessage = 'User disabled';
+          break;
+        case 'auth/user-not-found':
+          errormessage = 'User not found';
+          break;
+        case 'auth/wrong-password':
+          errormessage = 'Incorrect password';
+          break;
+          case 'auth/network-request-failed':
+            errormessage = 'Required internet';
+            break;
+            case 'auth/unknown':
+              errormessage='error occured please try again';
+              break;
+          default:
+            errormessage = Error.message;
+    }
+    setloading(true)
+    Toast.show({
+      type:'error',
+      text1:errormessage,
+      visibilityTime:4000
+    })
     setemail('')
     setpassword('')
-   })
+   }
 }
 else{
-    Alert.alert('fields are empty')
+  setloading(true)
+   Toast.show({
+    type:'error',
+    text1:'Field are Empty',
+    visibilityTime:3000,
+   })
 }
     }
+
+    const datafirestore=async(userId)=>{
+        try {
+          const usersRef = firestore().collection('users')
+          const querySnapshot = await usersRef.where('Email', '==', email).get()
+          if (querySnapshot.empty) {
+            const {Fullname,phnumber} =route.params;
+            console.log(Fullname,phnumber)
+            await firestore().collection('users').doc(userId).set({
+              Email: email,
+              Password: password,
+              Fullname:Fullname,
+              PhoneNumber:phnumber,
+              cart: [],
+              Address: [],
+              Defaultaddress: [],
+            })
+          }
+        } catch (error) {
+          console.error(error)
+        }
+  }
    
     const loginhandle=()=>{
-         login()
+      Keyboard.dismiss()
+      login()
+     
     }
+    const HandleEmailvalidation=()=>{
+      const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      if(regex.test(email)|| email.length==0){
+        return(
+         <Text></Text>
+      )    
+      }else{
+        return(
+     <Text style={styles.validtext}>Please enter a valid email address</Text>
+      )
+      }
+    }
+
+    const HandlePasswordvalidation = () => {
+      const minLength = password.length >=8 
+      const hasLowercase = /[a-z]/.test(password);
+      const hasUppercase = /[A-Z]/.test(password);
+      const hasDigit = /\d/.test(password);
+      const hasSpecialChar = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(password);
+    if(!minLength && password.length!=0){
+      return(
+      <Text style={styles.validtext}>Password must be contain 8 characters</Text>
+    )}
+    else if(!hasLowercase && password.length!=0){
+      return(
+      <Text style={styles.validtext}>Password must contain at least one small letter</Text>
+    )}
+   else if(!hasUppercase && password.length!=0){
+    return(
+    <Text style={styles.validtext}>Password must contain at least one capital letter</Text>
+  )}
+  else if(!hasDigit && password.length!=0){
+    return(
+    <Text style={styles.validtext}>Password must contain at least one digit</Text>
+  )}
+  else if(!hasSpecialChar && password.length!=0){
+    return(
+    <Text style={styles.validtext}>Password must be contain at least one special character</Text>
+  )}
+  else{
+    return(
+      <Text></Text>
+    )}
+  } 
+   const Apploader=()=>{
+    return(
+      <View style={[styles.loadercontainer,StyleSheet.absoluteFillObject]}>
+        <ActivityIndicator size={80}/>
+      </View>
+    )
+  }
+
   return (
     <View style={styles.logincontainer}>
       <Text style={styles.texthead}>Sign in</Text>
       <Text style={styles.subtext}>Sign into your account</Text>
+      <View style={styles.textinputcontainer}>
       <TextInput
-      placeholder='Email'
+      placeholderTextColor={'#478778'}
+      placeholder='Email' 
       value={email}
       style={styles.textinput}
       onChangeText={(text)=>setemail(text)}/>
+    <HandleEmailvalidation/>
        <TextInput
-      placeholder='Password'
-      value={password}
-      secureTextEntry={true}
-      onChangeText={(text)=>setpassword(text)}
-      style={styles.textinput}/>
+  placeholder='Password'
+  placeholderTextColor={'#478778'}
+  value={password}
+  secureTextEntry={true}
+  onChangeText={(text)=>setpassword(text)}
+  style={styles.textinput}/>
+  <HandlePasswordvalidation/>
+      </View>
       <TouchableOpacity style={styles.buttonlogin} onPress={()=>loginhandle()}>
         <Text style={styles.logintext}>Login</Text>
       </TouchableOpacity>
@@ -62,17 +186,7 @@ else{
             <Text style={styles.buttontextsignup}>Sign up</Text>
         </TouchableOpacity>
       </View>
-      <View>
-        <Text style={styles.textor}>-- OR --</Text>
-        <TouchableOpacity style={styles.skipbutton} onPress={()=>navigation.navigate('Bottomtabs', {
-        screen: 'Home',
-        params: { email:'',
-                 password:''
-        },
-      })}>
-            <Text style={styles.logintext}>skip Now</Text>
-        </TouchableOpacity>
-      </View>
+      {!loading && <Apploader/>}
     </View>
   )
 }
@@ -93,24 +207,27 @@ const styles = StyleSheet.create({
         borderWidth:1,
         paddingLeft:10,
         borderColor:'green',
-        marginTop:30
+        marginTop:15,
+        color:'#000',
+        alignSelf:'center'
     },
     texthead:{
         fontSize:25,
         fontWeight:'700',
         color:'#00D100',
-        marginBottom:2
+        marginBottom:2,
     },
     subtext:{
-        fontSize:18,
+        fontSize:19,
         fontWeight:'600',
+        color:'#478778',
     },
     buttonlogin:{
         width:200,
         height:50,
         backgroundColor:'#00cc00',
         borderRadius:8,
-        marginTop:30,
+        marginTop:20,
         justifyContent:'center',
         alignItems:'center'
     },
@@ -121,30 +238,34 @@ const styles = StyleSheet.create({
     },
     signupcontainer:{
         flexDirection:'row',
-        marginTop:10,
+        marginTop:20,
     },
     signuptext:{
         fontSize:18,
-        fontWeight:'500'
+        color:'#478778',
+        fontFamily:'NotoSansSundanese-SemiBold'
     },
     buttontextsignup:{
         fontSize:18,
         marginLeft:5,
-        fontWeight:'500'
+        fontWeight:'500',
+        color:'#478778'
     },
-    skipbutton:{
-        width:200,
-        height:50,
-        backgroundColor:'#00cc00',
-        justifyContent:'center',
-        alignItems:'center',
-        borderRadius:8,
-        marginTop:20
-    },
-    textor:{
-        fontSize:20,
-        fontWeight:'600',
-        marginTop:20,
-        alignSelf:'center'
-    }
+  
+  validtext:{
+    color:'#EE4B2B',
+     fontFamily:'NotoSansSundanese-Regular',
+   marginTop:3,
+   marginLeft:35
+  },
+  textinputcontainer:{
+    width:'100%',
+    marginTop:40
+  },
+  loadercontainer:{
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'rgba(0,0,0,0.3)',
+    zIndex:1
+  }
 })
